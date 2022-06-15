d5961cb09acbbbf256019639400c3d4d851a14b5
ngholland <105685359+ngholland@users.noreply.github.com>
2022-06-15 19:43:04 -0400

    Cleaned things up, removed some unnecessary lines

--- sigmav_ion_hh.py
+++ sigmav_ion_hh.py
@@ -10,16 +10,11 @@ def sigmav_ion_hh(Te):
 
 #   Te - Scalar, list, or np.array. Gives electron temperature (eV)
 
-  is_scalar=False
-  if type(Te) not in [np.ndarray, list]: #converts scalar input to iterable list
-    Te=[Te]
-    is_scalar=True
+  if type(Te) != np.ndarray: #converts scalar or list input to np.array
+    Te=np.array(Te)
   b = [-3.568640293666e+1, 1.733468989961e+1, -7.767469363538e+0, 
          2.211579405415e+0, -4.169840174384e-1, 5.088289820867e-2, 
         -3.832737518325e-3, 1.612863120371e-4, -2.893391904431e-6]
-  Te2=[e if e>.1 else .1 for e in Te] #Sets values to minimum .1 and maximum 1e5
-  np.array([e if e<2.01e4 else 2.01e4 for e in Te2]) #And converts to np.array
-  result=np.e**(poly(np.log(Te2),b))*1e-6
-  if is_scalar: #if the input was a scalar, converts result back to a scalar as well
-    result=result[0]
-  return result
+  Te2=np.maximum(Te,0.1) #Sets values to minimum .1 and maximum 2.01e4
+  Te2=np.minimum(Te2,2.01e4) 
+  return np.e**(poly(np.log(Te2),b))*1e-6
