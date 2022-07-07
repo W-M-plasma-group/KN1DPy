720a00416e6bb8b81c2add3b8c8e9d27fc7fd6c4
ngholland <105685359+ngholland@users.noreply.github.com>
2022-07-07 15:20:57 -0400

    Update create_vrvxmesh.py
    Fixed a few bugs. Lines 34 and down were accidentally indented to still be in the else statement. The IDL code's comments say that vx should have length 2*nv+1, but it looks like it only has length 2*nv? Not sure what that's about.

--- create_vrvxmesh.py
+++ create_vrvxmesh.py
@@ -1,4 +1,4 @@
-from cmath import sqrt
+#from cmath import sqrt     replaced with np.sqrt - nh
 from reverse.py import *
 import numpy as np
 
@@ -11,49 +11,42 @@ import numpy as np
 #
 # Gwendolyn Galleher 
 
-def create_VrVxMesh(nv, Ti, vx, Tnorm, ixE0, irE0, E0 = np.array([0.0]), Tmax = 0.0):
-    _Ti = Ti 
-    for k in range(0, np.size(E0) - 1):
-        if E0[k] > 0:
-            _Ti = np.array([_Ti, E0[k]])
+def create_VrVxMesh(nv, Ti, E0 = np.array([0.0]), Tmax = 0.0): # removed unused input parameters
+    _Ti = np.array(Ti) 
+    _Ti = np.concatenate([_Ti, E0[E0>0]]) 
     if Tmax > 0:
-        ii = np.argwhere(_Ti < Tmax)
-        count = np.size(ii)
-        if count > 0:
-            _Ti = _Ti[ii]
+        _Ti = _Ti[_Ti<Tmax] # simplified previous lines by replacing loops - nh
     maxTi = np.max(_Ti)
     minTi = min(_Ti)
     Tnorm = np.mean(_Ti)
     vmax = 3.5
-    if maxTi - minTi < 0.1 * maxTi:
-        v = np.arange(nv+1, float)
+    if maxTi - minTi <= 0.1 * maxTi: # changed < to <= like from IDL code - nh
+        v = np.arange(nv+1, float)*vmax/nv # added *vmax/nv from IDL code - nh
     else:
-        G = 2 * nv * sqrt(minTi / maxTi) / (1- sqrt(minTi / maxTi))
+        G = 2 * nv * np.sqrt(minTi / maxTi) / (1- np.sqrt(minTi / maxTi))
         b = vmax / (nv * (nv + G))
         a = G * b 
         v = a * np.arange(nv +1) + b * (np.arange(nv+1) ** 2)
 
         # Option: add velocity bins corresponding to E0 
-
-        for k in range(np.size(E0) - 1):
-            if E0[k] > 0.0:
-                v0 = sqrt(E0[k]/Tnorm)
-                ii = np.argwhere(v > v0)
-                count = np.size(ii)
-                if count > 0:
-                    v = np.array([v[0 : ii[0]], v0, v[ii[0] : v[np.size(v) - 1]]])
-                else: 
-                    v = [v, v0]
         
-        vr = v[1 : np.size(v) - 1]
-        vx = [-reverse(vr), vr]
+    v0=0 # this line was missing - nh
+    for k in range(np.size(E0)): # fixed range - nh
+        if E0[k] > 0.0:
+            v0 = np.sqrt(E0[k]/Tnorm)
+            ii = np.argwhere(v > v0).T[0] # argwhere has a weird output, but this should put it in a usable format - nh
+            if np.size(ii) > 0: # removed count variable
+                v = np.concatenate([v[0 : ii[0]], [v0], v[ii[0] : ]])
+            else: 
+                v = np.concatenate([v, v0]) # changed lines to np.concatenate - nh
+        
+    vr = v[1 : ] # fixed indexing - nh
+    vx = np.concatenate([-reverse(vr), vr]) 
 
-        ixE0 = np.argwhere(abs(vx) == v0)
-        count = np.size(ixE0)
-        if count == 1:
-            ixE0 = ixE0[0]
-        irE0 = np.argwhere(vr == v0)
-        count = np.size(irE0)
-        if count == 1:
-            irE0 = irE0[0]
-        return 
+    ixE0 = np.argwhere(abs(vx) == v0).T[0] # modified argwhere output - nh
+    if np.size(ixE0) == 1: # removed count variable; same changes made to following lines - nh
+        ixE0 = ixE0[0]
+    irE0 = np.argwhere(vr == v0).T[0]
+    if np.size(irE0) == 1:
+        irE0 = irE0[0]
+    return [vx,vr,Tnorm,ixE0,irE0] # changed to return outputs as a list
