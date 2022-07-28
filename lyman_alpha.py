a58bd0f58f542897466a84e3cb97bc8623ae78c4
Gwen <105458535+grgalleher@users.noreply.github.com>
2022-07-28 13:38:14 -0400

    first draft of lyman_alpha. I will come back and change the common block variables this is only temporary until we further discuss it. I also haven't checked it yet because files it calls are not done yet.

---
+++ lyman_alpha.py
@@ -0,0 +1,71 @@
+import numpy as np
+from create_jh_bscoef import Create_JH_BSCoef
+from JHR_Coef import JHR_Coef
+from NHSaha import NHSaha
+
+#     Computes Lyman-alpha emissivity (watts m^-3) given the local
+#  electron density, electron temperature, and ground-state
+#  neutral density.
+#
+#  Method:
+#     (1) Compute the local n=2 population density using the Johnson-Hinnov
+#         rate equations and coefficients [L.C.Johnson and E. Hinnov, J. Quant. 
+#         Spectrosc. Radiat. Transfer. vol. 13 pp.333-358]
+#     (2) Multiply by the n=2->1 spontaneous emission coefficient
+#     (3) Convert to watts/m^3
+
+def Lyman_Alpha(Density, Te, N0, photons = 0, create = 0, no_null = 0):
+    #________________________________________________________________________________
+    # Input:
+    #  	Density	- fltarr, electron density (=hydrogen ion density) (m^-3)
+    #  	Te	- fltarr, electron temperature (eV
+    #  	N0	- fltarr, ground state neutral density (m^-3)
+    #
+    # Keywords:
+    #	photons - returns emissivity in number of photons m^-3 s^-1
+    #	create	- if set, then create bi-cubic spline coefficients for
+    #		  interpolation of r0(p) r1(p) and save them in the
+    #		  default save set. 
+    #	No_Null	- if set, then rather than generate a NULL value when Density and Te
+    #                 are not null but still outside the data range, compute the rate based on the min or max
+    #		  data range values.
+    #________________________________________________________________________________
+    # History:
+    #    Coding by B. LaBombard  6/29/99
+    #    Coefficients from J. Terry's idl code JH_RATES.PRO
+    # variables in JH_coef common block - this is only temporary bc we havent finished discussing common blocks
+    Dknot = None
+    Tknot = None
+    LogR_BSCoef=None
+    LogS_BSCoef=None
+    LogAlpha_BSCoef=None
+    A_Lyman=None
+    A_Balmer=None
+    if create:
+        Create_JH_BSCoef()
+    if LogR_BSCoef is None:
+        # this is where old data is restored I don't entirely know how we want to do this yet or if we are doing this at all 
+        pass 
+    
+    # From Johnson-Hinnov, eq (11):
+    # n(2) =  ( r0(2) + r1(2) * n(1) / NHsaha(1) ) * NHsaha(2)
+    if np.size(Density) != np.size(Te):
+        raise Exception('Number of elements of Density and Te are different!')
+    if np.size(Density) != np.size(Te):
+        raise Exception(' Number of elements of Density and N0 are different! ')
+    result = Density ; result[:] = 1.0e32
+    photon = result 
+    r02 = JHR_Coef(Density, Te, 0, 2, no_null = no_null)
+    r12 = JHR_Coef(Density, Te, 1, 2, no_null = no_null)
+    NHSaha1 = NHSaha(Density, Te, 1)
+    NHSaha2 = NHSaha(Density, Te, 2)
+    for i in range(0, np.size(Density)):
+        if 0 < N0 < 1e32 and r02 < 1.0e32 and r12 < 1.0e32 and NHSaha1 < 1.0e32 and NHSaha2 < 1.0e32:
+            ok = np.append(ok, i)
+    count = np.size(ok)
+    ok = np.astype(int)
+    if count > 0:
+        for i in range(0, np.size(ok)):
+            photons[i] = A_Balmer[0] * ( r02[i] + r12[i] * N0[i] / NHSaha1[i] ) * NHSaha2[i]
+            result[i] = 13.6057 * ( 0.25 - 1.0 / 9.0 ) * photons[i] * 1.6e-19
+    return result
