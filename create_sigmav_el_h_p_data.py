3ff92a5f5647781ea421cdde4f723bffd2e1cdf9
ngholland <105685359+ngholland@users.noreply.github.com>
2023-06-12 15:49:44 -0400

    Update create_sigmav_el_h_p_data.py
    Added import line for sigma_function. Also updated lines to save results to a file.

--- create_sigmav_el_h_p_data.py
+++ create_sigmav_el_h_p_data.py
@@ -1,13 +1,14 @@
 import numpy as np
 from make_sigmav import make_sigmav
 from sval import sval
+from sigma_el_p_h import Sigma_EL_P_H # added import line for sigma_function - nh
 
 def create_sigmav_el_h_p_data():
 
     #Creates a 2D SigmaV data table in particle energy and target temperature
     #   and saves it as a save set.
     
-    sigma_function='Sigma_EL_P_H'
+    sigma_function=Sigma_EL_P_H
 
     mE=50
     Emin, Emax = 0.1, 2e4
@@ -28,6 +29,7 @@ def create_sigmav_el_h_p_data():
     Ln_E_Particle=np.log(E_Particle)
     Ln_T_Target=np.log(T_Target)
 
-    print('Saving results in file: /home/labombard/edge/neutrals/sigmav_el_p_h_data.dat')
+    #print('Saving results in file: /home/labombard/edge/neutrals/sigmav_el_h_p_data.dat') # orginal file location in IDL version
+    print('Saving results in file: sigmav_el_h_p_data.npz') # saves in current directory for now - nh
 
-    #   SAVE FILE HERE
+    np.savez('sigmav_el_h_p_data',Ln_E_Particle=Ln_E_Particle,Ln_T_Target=Ln_T_Target,SigmaV=sigmav)
