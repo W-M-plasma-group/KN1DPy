225c5bd0791c0c094e336fa8032aa38c022c6c7e
ngholland <105685359+ngholland@users.noreply.github.com>
2023-07-19 16:42:34 -0400

    Update create_kinetic_h_mesh.py
    Added common block input to pass to JHS_coef(), fixed indexing / concatenation bugs

--- create_kinetic_h_mesh.py
+++ create_kinetic_h_mesh.py
@@ -6,7 +6,7 @@ from collrad_sigmav_ion_h0 import collrad_sigmav_ion_h0
 from sigma_cx_h0 import sigma_cx_h0
 from JHS_coef import JHS_coef
 
-def create_kinetic_h_mesh(nv, mu, x, Ti, Te, n, PipeDia, E0 = 0, ixE0 = 0 ,irE0 = 0,fctr = 1):
+def create_kinetic_h_mesh(nv, mu, x, Ti, Te, n, PipeDia, E0 = 0, ixE0 = 0 ,irE0 = 0,fctr = 1, g=None): # added common block input
     # add these lines to improve velocity space resolution (use with care):
     nv = 20
     # E0[.1]
@@ -33,12 +33,12 @@ def create_kinetic_h_mesh(nv, mu, x, Ti, Te, n, PipeDia, E0 = 0, ixE0 = 0 ,irE0 
     #  dGamma/dx=-nH*RR = v0 dnH/dx = -nH*RR - these two lines are commented in the original code I dont understand them
     #  d ln(nH)/dx = -RR/v0 = dY/dx
 
-    Y = np.zeros(np.shape(nx))
+    Y = np.zeros(nx) # fixed typo
     for k in range(1, nx): 
         Y[k] = Y[k-1] - (x[k] - x[k-1] ) * 0.5 * (RR[k] + RR[k-1]) / v0
     
     # Find x location where Y = -5, i.e. where nH should be down by exp(-5)
-    interpfunc = interpolate.interp1d(Y, x, kind = 'linear')
+    interpfunc = interpolate.interp1d(Y, x, kind = 'linear', bounds_error=False, fill_value="extrapolate") # previous version caused error on next line
     xmaxH = np.minimum(interpfunc(-5), max(x))
     xminH = x[0]
 
@@ -57,7 +57,7 @@ def create_kinetic_h_mesh(nv, mu, x, Ti, Te, n, PipeDia, E0 = 0, ixE0 = 0 ,irE0 
     PipeDiafine = interpfunc(xfine)
 
     # Set up a vx, vr mesh based on raw data to get typical vx, vr values 
-    vx, vr, Tnorm = create_VrVxMesh(nv, Tifine)
+    vx, vr, Tnorm = create_VrVxMesh(nv, Tifine)[:3] # only take first 3 outputs
     vth = np.sqrt( 2 * q * Tnorm / (mu * mH))
     minVr = vth * min(vr)
     minE0 = 0.5 * mH * minVr * minVr / q
@@ -65,20 +65,20 @@ def create_kinetic_h_mesh(nv, mu, x, Ti, Te, n, PipeDia, E0 = 0, ixE0 = 0 ,irE0 
     # Estimate interaction rate with side walls
     nxfine = np.size(xfine)
     gamma_wall = np.zeros(nxfine)
-    print(PipeDiafine)
-    for k in range(0, nxfine):
-        if PipeDiafine > 0:
-            gamma_wall[k] = 2 * max(vr) * vth / PipeDiafine
+    #print(PipeDiafine)
+    for k in range(0, nxfine): # fixed typo on next two lines
+        if PipeDiafine[k] > 0:
+            gamma_wall[k] = 2 * max(vr) * vth / PipeDiafine[k]
     
     # Estimate total reaction rate, including charge exchange and elastic scattering, and interaction with side walls 
     if Use_Collrad_Ionization:
         ioniz_rate = collrad_sigmav_ion_h0(nfine, Tefine)
     else:
         if JH:
-            ioniz_rate = JHS_coef(nfine, Tefine, no_null = True)
+            ioniz_rate = JHS_coef(nfine, Tefine, no_null = True, g=g) # added common block
         else:
             ioniz_rate = sigmav_ion_h0(Tefine)
-    RR = nfine * ioniz_rate + nfine * sigma_cx_h0(Tifine, np.array([minE0] * np.size(minE0))) + gamma_wall
+    RR = nfine * ioniz_rate + nfine * sigma_cx_h0(Tifine, np.array([minE0] * nxfine)) + gamma_wall # replaced size(nxfine) with nxfine
 
     # Compute local maximum grid spacing dx_max = 2 
     big_dx = 0.02 * fctr 
@@ -88,7 +88,7 @@ def create_kinetic_h_mesh(nv, mu, x, Ti, Te, n, PipeDia, E0 = 0, ixE0 = 0 ,irE0 
     xpt = xmaxH
     xH = np.array([xpt])
     while xpt > xminH:
-        xH = np.concatenate(xpt, xH)
+        xH = np.concatenate([np.array([xpt]), xH]) # put xpt in array to fix concatenation error
         interpfunc = interpolate.interp1d(xfine, dx_max)
         dxpt1 = interpfunc(xpt)
         dxpt2 = dxpt1
@@ -101,7 +101,7 @@ def create_kinetic_h_mesh(nv, mu, x, Ti, Te, n, PipeDia, E0 = 0, ixE0 = 0 ,irE0 
         dxh_max = 0.0005 # JWH: 0.0015 should be sufficient for D3D because scale lengths are 2.5x larger
         dxpt = min([dxpt1, dxpt2, dxh_max])
         xpt = xpt - dxpt 
-    xH = np.concatenate(xminH, xH[0:np.size(xH) - 2])
+    xH = np.concatenate([np.array([xminH]), xH[0:np.size(xH) - 2]]) # put xminH in array to fix concatenation error
     # if xH[1] - xH[0] > 0.5 * big_dx:
     #   xH = np.concatenate(xH[0], xH[2:])
 
@@ -113,5 +113,5 @@ def create_kinetic_h_mesh(nv, mu, x, Ti, Te, n, PipeDia, E0 = 0, ixE0 = 0 ,irE0 
     neH = interpfunc(xH)
     interpfunc = interpolate.interp1d(xfine, PipeDiafine)
     PipeDiaH = interpfunc(xH)
-    vx, vr, Tnorm = create_VrVxMesh(nv, TiH)
+    vx, vr, Tnorm = create_VrVxMesh(nv, TiH)[:3] # only take first 3 outputs
     return xH, TiH, TeH, neH, PipeDiaH, vx, vr, Tnorm
