a207a2a7adf1773cc8f95da8cdb7ed9edd4ce52b
ngholland <105685359+ngholland@users.noreply.github.com>
2023-08-02 14:49:15 -0400

    Update balmer_alpha.py
    Updated to use current implementation of common block variables, as well as restoring saved data

--- balmer_alpha.py
+++ balmer_alpha.py
@@ -14,7 +14,7 @@ from JHR_Coef import JHR_Coef
 #   (2) Multiply by the n=3->2 spontaneous emission coefficient
 #   (3) Convert to watts/m^3
 
-def Balmer_Alpha(Density, Te, N0, photons = 0, create = 0, no_null = 0):
+def Balmer_Alpha(Density, Te, N0, photons = 0, create = 0, no_null = 0, g=None):
     #________________________________________________________________________________
     # Input:
     #  	Density	- fltarr, electron density (=hydrogen ion density) (m^-3)
@@ -35,18 +35,26 @@ def Balmer_Alpha(Density, Te, N0, photons = 0, create = 0, no_null = 0):
     #    Coefficients from J. Terry's idl code JH_RATES.PRO
     
     # variables in JH_coef common block - this is only temporary bc we havent finished discussing common blocks
-    Dknot = None
-    Tknot = None
-    LogR_BSCoef=None
-    LogS_BSCoef=None
-    LogAlpha_BSCoef=None
-    A_Lyman=None
-    A_Balmer=None
+    Dknot = g.JH_Coef_DKnot
+    Tknot = g.JH_Coef_TKnot
+    LogR_BSCoef=g.JH_Coef_LogR_BSCoef
+    LogS_BSCoef=g.JH_Coef_LogS_BSCoef
+    LogAlpha_BSCoef=g.JH_Coef_LogAlpha_BSCoef
+    A_Lyman=g.JH_Coef_A_Lyman
+    A_Balmer=g.JH_Coef_A_Balmer
     if create:
         Create_JH_BSCoef()
     if LogR_BSCoef is None:
-        # this is where old data is restored I don't entirely know how we want to do this yet or if we are doing this at all 
-        pass 
+        # this is where old data is restored 
+        s=np.load('jh_bscoef.npz')
+        Dknot=s['DKnot']
+        Tknot=s['TKnot']
+        order=s['order']
+        LogR_BSCoef=s['LogR_BSCoef']
+        LogS_BSCoef=s['LogS_BSCoef']
+        LogAlpha_BSCoef=s['LogAlpha_BSCoef']
+        A_Lyman=s['A_Lyman']
+        A_Balmer=s['A_Balmer']
 
     # From Johnson-Hinnov, eq (11):
     # n(3) = ( r(0) + r1(3) * n(1) / NHsaha(1) ) * NHsaha(3)
@@ -57,8 +65,8 @@ def Balmer_Alpha(Density, Te, N0, photons = 0, create = 0, no_null = 0):
         raise Exception(' Number of elements of Density and N0 are different! ')
     result = Density ; result[:] = 1.0e32
     photons = result 
-    r03 = JHR_Coef(Density, Te, 0, 3, no_null = no_null)
-    r13 = JHR_Coef(Density, Te, 1, 3, no_null = no_null)
+    r03 = JHR_Coef(Density, Te, 0, 3, no_null = no_null, g=g)
+    r13 = JHR_Coef(Density, Te, 1, 3, no_null = no_null, g=g)
     NHSaha1 = NHSaha(Density, Te, 1)
     NHSaha3 = NHSaha(Density, Te, 3)
     for i in range(0, np.size(Density)):
