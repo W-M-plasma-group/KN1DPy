4fa80c538a0eb37332412cdae9638749f82f7826
carlobec23 <carlobec23@gmail.com>
2024-07-25 17:25:35 -0500

    Added: credentials_spreadsheets.json file and Updated: Test.py to connect to a google spreadsheet

--- Test.py
+++ Test.py
@@ -1,2 +1,11 @@
+import gspread
+from google.oauth2.service_account import Credentials
+scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+creds  = Credentials.from_service_account_file("kn1d-project-credentials-spreadsheets-135c1abb3187.json",scopes=scopes)
+client = gspread.authorize(creds)
 
-print('hello')
+sheet_id = "1bdl3KibDaG6Yxah7OL7B39nl0ReJws950qINjtHt84g"
+sheet = client.open_by_key(sheet_id)
+
+values_list = sheet.sheet1.row_values(1)
+print(values_list)
